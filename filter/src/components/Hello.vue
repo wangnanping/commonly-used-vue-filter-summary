02674e404145488e459d646168c4a39aa96577ab
王南坪 <nan-ping.wang@utsoft.cn>
2017-09-04 11:18:01 +0800

    多条件过滤器查询

--- filter/src/components/Hello.vue
+++ filter/src/components/Hello.vue
@@ -96,31 +96,25 @@ export default {
   computed: {
     newUsers: function() {
       let self = this;
-      // if (self.searchValue) {
-        return self.dataList.filter(function(data) {
-          return data.Star.indexOf(self.searchValue) !== -1;
-          // switch (self.searchValue) {
-          //   case (data.name.indexOf(self.searchValue) !== -1):
-          //     return data
-          //     break
-          //   case (data.Birthday.indexOf(self.searchValue) !== -1):
-          //     return data
-          //     break
-          //   case (data.address.indexOf(self.searchValue) !== -1):
-          //     return data
-          //     break
-          //   case (data.Fruits.indexOf(self.searchValue) !== -1):
-          //     return data
-          //     break
-          //   case (data.Star.indexOf(self.searchValue) !== -1):
-          //     return data
-          //     break
-          // }
-        })
-      // } else {
-      //   return self.dataList
-      // }
+      return self.dataList.filter(function(data) {
+        if (data.Star.indexOf(self.searchValue) != -1) {
+          return data
+          return false;
+        } else if (data.name.indexOf(self.searchValue) != -1) {
+          return data
+          return false;
+        } else if (data.Birthday.indexOf(self.searchValue) != -1) {
+          return data
+          return false;
+        } else if (data.address.indexOf(self.searchValue) != -1) {
+          return data
+          return false;
+        } else if (data.Fruits.indexOf(self.searchValue) != -1) {
+          return data
+          return false;
+        }
 
+      })
     }
   },
   filters: {
