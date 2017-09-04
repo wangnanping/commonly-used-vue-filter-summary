4580d10df1ba484a7450da8c64a0f46be211fd51
王南坪 <nan-ping.wang@utsoft.cn>
2017-09-04 10:29:13 +0800

    添加一个简单的本地搜索过滤name

--- filter/src/components/Hello.vue
+++ filter/src/components/Hello.vue
@@ -7,6 +7,30 @@
     <p>5.数字添加逗号-->{{12345678.124 | digitalInterval}}</p>
     <p>6.没有自动添加小数点0.00 有一位小数添加 0.X0 两位 不做向上向下取整-->{{1 | decimalRetention}}</p>
     <p>7.组合 5 6 -->{{123123.1231 | decimalRetention | digitalInterval}}</p>
+    <div>
+      8.输入搜索值，过滤参数值，如果是配合后台就应该传参在后台做数据处理返回显示，这里做的是本地的
+      <input type="text" v-model="searchValue">
+      <table class="table">
+        <thead>
+          <tr>
+            <td>名字</td>
+            <td>生日</td>
+            <td>地址</td>
+            <td>喜欢的水果</td>
+            <td>喜欢的电影明星</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item,index) in newUsers" :key="index">
+            <td>{{item.name}}</td>
+            <td>{{item.Birthday}}</td>
+            <td>{{item.address}}</td>
+            <td>{{item.Fruits}}</td>
+            <td>{{item.Star}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -17,12 +41,64 @@ import booleanFormatter from '../filters/booleanFormatter' //判断true=>渲染y
 import percentageFormatter from '../filters/percentageFormatter' //参数为保留小数点位数 四舍五入 总1 占得比重 
 import digitalInterval from '../filters/digitalInterval' //数字添加逗号
 import decimalRetention from '../filters/decimalRetention' //自动添加小数点0.00 两位 不做向上向下取整
-
+// import conditions from '../filters/conditions'
 export default {
   name: 'hello',
   data() {
     return {
-
+      dataList: [
+        {
+          name: '王先生',
+          Birthday: '1994/9/7',
+          address: '成都天府二街',
+          Fruits: '苹果',
+          Star: '杰森斯坦森'
+        },
+        {
+          name: '张晓明',
+          Birthday: '2000/4/2',
+          address: '宜宾大观楼',
+          Fruits: '香蕉',
+          Star: '霍建华'
+        },
+        {
+          name: '猪小明',
+          Birthday: '1959/4/2',
+          address: '成都天府一街',
+          Fruits: '菠萝',
+          Star: '周星驰'
+        },
+        {
+          name: '佟大为',
+          Birthday: '2049/4/2',
+          address: '成都天府二街',
+          Fruits: '苹果',
+          Star: '甑子丹'
+        },
+        {
+          name: '贺小书',
+          Birthday: '1959/12/1',
+          address: '成都天府三街',
+          Fruits: '尿',
+          Star: '周星驰'
+        },
+        {
+          name: '猪小明',
+          Birthday: '2012/4/2',
+          address: '成都天府四街',
+          Fruits: '菠萝',
+          Star: '周星驰'
+        }
+      ],
+      searchValue: ''
+    }
+  },
+  computed: {
+    newUsers: function() {
+      let self = this;
+      return self.dataList.filter(function(data) {
+        return data.name.indexOf(self.searchValue) !== -1;
+      })
     }
   },
   filters: {
@@ -31,7 +107,8 @@ export default {
     booleanFormatter,
     percentageFormatter,
     digitalInterval,
-    decimalRetention
+    decimalRetention,
+    // conditions
   }
 }
 </script>
@@ -39,5 +116,21 @@ export default {
 <style scoped>
 p {
   text-align: left;
+}
+
+div {
+  text-align: left;
+}
+
+table {
+  border: 1px solid #EEE;
+}
+
+table thead tr td {
+  border: 1px solid #EEE;
+}
+
+table tbody tr td {
+  border: 1px solid #EEE;
 }
 </style>
